12520b0e8513ad1bfd46696539cdbb829e3f0ce3
Rick <fishorb@gmail.com>
2016-12-20 20:59:46 +0000

    success!

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
+var moment = require("moment");
 var express = require("express");
 var app = express();
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.use('', function (req, res) {
+    var input = decodeURIComponent(req['path'].substring(1));
+    var unix = null;
+    var natural = null;
+    // check if unix date or natural date
+    if (moment(input, "X", true).isValid()){
+        var theDate = moment(input, "X", true);
+    } else if (moment(input, ["MMMM D, YYYY", "MMMM DD, YYYY"], true).isValid()){
+        var theDate = moment(input, ["MMMM D, YYYY", "MMMM DD, YYYY"], true)
+    }
+    // if a date, format output values
+    if (theDate) {
+        unix = theDate.format('X');
+        natural = theDate.format("MMMM D[,] YYYY");
+    }
+    
+    // return dates
+    var theDates = {
+        "unix": unix,
+        "natural": natural
+    };
+    res.send(theDates);
 })
 
 app.listen(8080, function () {
